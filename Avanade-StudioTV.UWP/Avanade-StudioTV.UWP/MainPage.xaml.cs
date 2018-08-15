b87628fb7021585ddb318e70008c2b7cae86ee9d
Ahmed Khan <ahmedkhan25@gmail.com>
2018-08-14 23:04:29 -0500

    Added UWP project (Part 2)

---
+++ Avanade-StudioTV.UWP/Avanade-StudioTV.UWP/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Xamarin.Forms.Platform.UWP;
+using Xamarin;
+using Xamarin.Forms;
+using Avanade_StudioTV;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Avanade_StudioTV.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Avanade_StudioTV.App());
+        }
+    }
+}
